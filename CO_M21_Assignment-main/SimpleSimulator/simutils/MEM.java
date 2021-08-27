c9a3d071fe62e59db176b18e90da70abbe4ee8a5
rksolani <rishabh18257@iiitd.ac.in>
2021-08-27 20:03:14 +0530

    Update MEM.java

--- CO_M21_Assignment-main/SimpleSimulator/simutils/MEM.java
+++ CO_M21_Assignment-main/SimpleSimulator/simutils/MEM.java
@@ -1,3 +1,25 @@
 package simutils;
 
 
+import java.util.Scanner;
+
+public class MEM {
+    
+    public static int MEMSIZE = 256;
+    public static int currentsize = 0;
+    public static String[] memory = new String[MEMSIZE];
+    public static void initialize(){
+        
+        for(int i=0;i<MEMSIZE;i++) memory[i]="0000000000000000";
+        Scanner sc = new Scanner(System.in);
+        while(sc.hasNextLine())
+            memory[currentsize++]=sc.nextLine();
+    }
+    public static void dump(){
+        for(String i: memory)
+            System.out.println(i);
+    }
+    public static void main(String[] args){
+
+    }
+}
