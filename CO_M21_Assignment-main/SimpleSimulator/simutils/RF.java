d83420c0c7696d2bd1a858ba5e3790b5f61f23ad
Rishabh Solani <rishabh18257@iiitd.ac.in>
2021-08-27 23:49:04 +0530

    Update RF.java

--- CO_M21_Assignment-main/SimpleSimulator/simutils/RF.java
+++ CO_M21_Assignment-main/SimpleSimulator/simutils/RF.java
@@ -5,6 +5,7 @@ import java.util.HashMap;
 public class RF {
     public static String flag="0000000000000000";
     public static HashMap<String,String>Register =new HashMap<>(){{
+        
         put("000","0000000000000000");
         put("001","0000000000000000");
         put("010","0000000000000000");
@@ -16,6 +17,7 @@ public class RF {
     }};
 
     public static void setflag(String type){
+        
         switch (type){
             case "e": setRegister("111","0000000000000001");break;
             case "g": setRegister("111","0000000000000010");break;
@@ -26,20 +28,25 @@ public class RF {
         flag=getregister("111");
     }
     public static void resetflag(){
+        
         flag= getregister("111");
         setRegister("111","0000000000000000");
     }
     public static String getflag(){
+        
         return flag;
     }
 
     public static String getregister(String s){
+        
         return Register.get(s);
     }
     public static void setRegister(String s, String value){
+        
         Register.put(s,value);
     }
     public static void main(String[] args){
+        
         setflag("e");
         System.out.println(getregister("111"));
     }
